588c25d841cdd7c024309947d7f92d02505f95fe
devYeonjun <alcls0026@gmail.com>
2022-05-13 23:59:05 +1200

    Add two-way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    // Single state
+	// Single state
 	// const [userInput, setUserInput] = useState({
 	// 	enteredTitle: "",
 	// 	enteredAmount: "",
@@ -60,12 +60,31 @@ const ExpenseForm = () => {
 		// });
 	};
 
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate),
+		};
+
+        console.log(expenseData)
+		setEnteredTitle("");
+		setEnteredAmount("");
+		setEnteredDate("");
+	};
+
 	return (
-		<form>
+		<form onSubmit={submitHandler}>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
@@ -73,6 +92,7 @@ const ExpenseForm = () => {
 						type="number"
 						min="0.01"
 						step="0.01"
+						value={enteredAmount}
 						onChange={amountChangeHandler}
 					/>
 				</div>
@@ -82,6 +102,7 @@ const ExpenseForm = () => {
 						type="date"
 						min="2022-01-01"
 						step="2022-12-31"
+						value={enteredDate}
 						onChange={dateChangeHandler}
 					/>
 				</div>
